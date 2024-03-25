1030e9e29bbe70a6b6d3f7e604ecf07e451f7a17
PeterSlovak <xslovakp3@stuba.sk>
2024-03-25 14:44:42 +0100

    some EDA

--- projekt.R
+++ projekt.R
@@ -3,8 +3,8 @@ library(magrittr)
 library(ggplot2)
 getwd() 
 #setwd("") #set working directory
-#read data https://www.kaggle.com/datasets/syedanwarafridi/vehicle-sales-data
-data <- read.csv("car_prices.csv", header = TRUE, sep = ",")
+
+data <- read.csv("AB_NYC_2019.csv", header = TRUE, sep = ",")
 
 
 View(data)
@@ -18,6 +18,92 @@ column_types_count
 
 complete_cases_count <- sum(complete.cases(data))
 table(complete.cases(data))
+
+ggplot(data, aes(x = price)) +
+  geom_histogram(binwidth = 10, fill = "blue", color = "black") +
+  labs(title = "Price Distribution Histogram", x = "Price", y = "Frequency")
+
+ggplot(data, aes(x = neighbourhood_group, y = price, fill = neighbourhood_group)) +
+  geom_boxplot() +
+  labs(title = "Price Distribution by Neighbourhood Group", x = "Neighbourhood Group", y = "Price")
+
+# just all columns
+lm_model <- lm(price ~ minimum_nights + number_of_reviews + reviews_per_month + calculated_host_listings_count + availability_365, data = data)
+summary(lm_model)
+
+residuals <- model$residuals
+RSS <- sum((residuals-mean(residuals))^2)
+RSS
+RMSE <- sqrt(mean(residuals^2))
+RMSE
+
+data %>% 
+  select(host_name, last_review, neighbourhood_group, price) %>%
+  group_by(neighbourhood_group) %>% 
+  summarise(mean_price = mean(price),
+            min_price = min(price),
+            max_price = max(price),
+            number_of_records = n())
+
+data %>%
+  select(host_name, last_review, neighbourhood_group, price) %>%
+  group_by(neighbourhood_group) %>%
+  summarise(mean_price = mean(price),
+            min_price = min(price),
+            max_price = max(price),
+            number_of_records = n()) %>%
+  ggplot(aes(x = neighbourhood_group, y = mean_price)) +
+  geom_col() +
+  labs(title = "Mean Price by Neighbourhood Group", x = "Neighbourhood Group", y = "Mean Price")
+
+
+
+
+#-----Helper functions-----
+panel.cor <- function(x,y, digits=2, prefix="", cex.cor){
+  usr <- par("usr")
+  on.exit(par(usr))
+  par(usr = c(0,1,0,1))
+  r <- abs(cor(x,y,use="complete.obs"))
+  txt <- format(c(r,0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt,sep="")
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor * (1 + r) / 2)
+}
+
+panel.hist <- function(x, ...){
+  usr <- par("usr")
+  on.exit(par(usr))
+  par(usr = c(usr[1:2],0,1.5))
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks
+  nB <- length(breaks)
+  y <- h$counts
+  y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col="white",...)
+}
+
+panel.lm <- function(x,y,col = par("col"), bg = NA, pch = par("pch"),
+                     cex = 1, col.smooth = "blue",...){
+  points(x,y,pch=pch, col=col(alpha = 0.1), bg=bg, cex=cex)
+  abline(stats::lm(x ~ y), col = "steelblue", ...)
+} 
+
+
+pairs(data[, c("price", "minimum_nights", "number_of_reviews", "reviews_per_month", "calculated_host_listings_count", "availability_365")],
+      upper.panel = panel.cor, 
+      diag.panel = panel.hist, 
+      lower.panel = panel.lm)
+
+
+
+
+
+
+
+
+
+
 
 # count classifier values in column transmission
 data %>% count(transmission)
