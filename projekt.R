1031c21432be5b781b84574a5963b6f63f60a0ec
PeterSlovak <xslovakp3@stuba.sk>
2024-03-27 09:47:53 +0100

    salary dataset

--- projekt.R
+++ projekt.R
@@ -259,3 +259,62 @@ data %$%
          upper.panel= NULL, 
          diag.panel=panel.hist,
          lower.panel=panel.smooth)
+
+
+data <- read_csv("salary_prediction_data.csv", col_names = TRUE)
+data
+
+# one hot encode Gender
+
+data <- data
+data$Gender <- as.factor(data$Gender)
+data <- data
+data$Gender <- as.numeric(data$Gender)
+data$Gender <- as.factor(data$Gender)
+data
+
+data$Gender <- as.factor(data$Gender)
+data$Education <- as.factor(data$Education)
+
+# Create the pairs plot
+ggpairs(data, 
+        columns = c("Salary", "Experience", "Location", "Job_Title", "Age", "Gender", "Education"),
+        upper = list(continuous = "cor"),
+        lower = list(combo = "box"),
+        diag = list(continuous = "barDiag", discrete = "barDiag"))
+
+# encode 
+#-----Helper functions-----
+panel.cor <- function(x,y, digits=2, prefix="", cex.cor){
+  usr <- par("usr")
+  on.exit(par(usr))
+  par(usr = c(0,1,0,1))
+  r <- abs(cor(x,y,use="complete.obs"))
+  txt <- format(c(r,0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt,sep="")
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor * (1 + r) / 2)
+}
+
+panel.hist <- function(x, ...){
+  usr <- par("usr")
+  on.exit(par(usr))
+  par(usr = c(usr[1:2],0,1.5))
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks
+  nB <- length(breaks)
+  y <- h$counts
+  y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col="white",...)
+}
+
+panel.lm <- function(x, y, col = par("col"), bg = NA, pch = par("pch"),
+                     cex = 1, col.smooth = "blue", ...) {
+  points(x, y, pch = pch, col = col, bg = bg, cex = cex)
+  abline(stats::lm(x ~ y), col = "steelblue", ...)
+} 
+pairs(~ Salary + Experience + Age, 
+      data = data,
+      upper.panel= NULL, 
+      diag.panel = panel.hist,
+      lower.panel = panel.lm)
